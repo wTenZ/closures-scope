d18d9bae870abdfcf70679ccb6ed8fc34a17a746
TenZ <08wTenZ@gmail.com>
2023-03-09 07:09:24 -0500

    reasignacion y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,30 @@
+var firstName; //undefned
+
+firstName = 'Oz';
+console.log(firstName);
+var lastName = 'ap'
+lastName = 'apellido';
+console.log(lastName);
+
+var secondName = 'T'; //declarando / asignando
+var secondName = 'Tatsu'; //reasignando
+console.log(secondName);
+
+//let
+let fruit = 'mizu'; //declarando / asignando
+fruit = 'pika'; //reasignando
+console.log(fruit)
+/*let fruit = 'nika'; //no se puede redeclarar*/
+
+//const
+const anima = 'dog'; //declaring and signing
+/* anima = 'cat' //cant reassigning or redeclaring*/
+console.log(anima)
+
+const car = []
+
+car.push('one');
+console.log(car)
+
+car.pop()
+console.log(car)
