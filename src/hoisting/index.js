0596ef9629818af3bf5c834e3ff3dfa622fe94eb
TenZ <08wTenZ@gmail.com>
2023-03-11 06:16:22 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+//with variables
+console.log(nameOfDog);
+var nameOfDog = 'Oz'
+console.log(nameOfDog);
+
+//with functions
+typeOz();
+function typeOz(){
+    console.log(`${doggie} is a doggie`);
+}
+
+var doggie = 'Oz'
+
+//
